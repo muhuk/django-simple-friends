a9f14a5ba87410eb0b27f7fab03f65f5d6c6c225
Atamert Ölçgen <muhuk@muhuk.com>
2010-03-15 22:01:27 +0200

    Added compilation of PO files to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
+import os
 from distutils.core import setup
+from django.core.management.commands.compilemessages import compile_messages
 from friends import __version__, __maintainer__, __email__
+
+
+def compile_translations():
+    curdir = os.getcwdu()
+    os.chdir(os.path.join(os.path.dirname(__file__), 'friends'))
+    compile_messages()
+    os.chdir(curdir)
+compile_translations()
+
 
 license_text = open('LICENSE.txt').read()
 long_description = open('README.rst').read()
+
 
 setup(
     name = 'django-simple-friends',
     version = __version__,
     url = 'http://github.com/muhuk/django-simple-friends',
-    author = __maintainer__.encode('utf8'),
+    author = __maintainer__,
     author_email = __email__,
     license = license_text,
     packages = ['friends', 'friends.templatetags'],
