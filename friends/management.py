7ea02fe374e63ca92d3c3615113aff033f14d775
Atamert Ölçgen <muhuk@muhuk.com>
2013-02-23 13:55:11 +0800

    Refactored post_syncdb handler, fixes #36

--- friends/management.py
+++ friends/management.py
@@ -1,16 +1,28 @@
 from django.db.models.signals import post_syncdb
+from django.contrib.auth.models import User
 import models
-#from django.contrib.auth.models import User
-import signals
+
+
+def post_syncdb_handler(sender, app, created_models, verbosity, **kwargs):
+    if verbosity >= 1:
+        print "Creating Friendship & UserBlocks models for existing users..."
+    user_ids = User.objects.values_list('pk', flat=True).order_by('pk')
+    total = user_ids.count()
+    batch_size = 2
+    for start in range(0, total, batch_size):
+        friendships = []
+        user_blocks = []
+        for user_id in user_ids[start:min(start + batch_size, total)]:
+            friendships.append(models.Friendship(user_id=user_id))
+            user_blocks.append(models.UserBlocks(user_id=user_id))
+        models.Friendship.objects.bulk_create(friendships)
+        models.UserBlocks.objects.bulk_create(user_blocks)
+    if verbosity >= 2 and total:
+        print "Created Friendship & UserBlocks for {0} users.".format(total)
 
 
 post_syncdb.connect(
-    signals.create_friendship_instance_post_syncdb,
+    post_syncdb_handler,
     sender=models,
-    dispatch_uid='friends.signals.create_friendship_instance_post_syncdb',
-)
-post_syncdb.connect(
-    signals.create_userblock_instance_post_syncdb,
-    sender=models,
-    dispatch_uid='friends.signals.create_userblock_instance_post_syncdb',
+    dispatch_uid='friends.signals.post_syncdb',
 )
