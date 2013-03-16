44cc17b31bafd0979712774cfcf0e324946f573c
Atamert Ölçgen <muhuk@muhuk.com>
2013-03-16 22:58:06 +0800

    Added Django as a dependency for package installation

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 import os, sys
-from distutils.core import setup
-from django.core.management.commands.compilemessages import compile_messages
+from setuptools import setup
 from friends import __version__, __maintainer__, __email__
+
 
 
 def compile_translations():
     curdir = os.getcwdu()
     os.chdir(os.path.join(os.path.dirname(__file__), 'friends'))
-    try:
-        compile_messages(stderr=sys.stderr)
-    except TypeError:
-        # compile_messages doesn't accept stderr parameter prior to 1.2.4
-        compile_messages()
+    compile_messages(stderr=sys.stderr)
     os.chdir(curdir)
-compile_translations()
+if sys.argv[0] == 'setup.py':
+    from django.core.management.commands.compilemessages import \
+        compile_messages
+    compile_translations()
 
 
 license_text = open('LICENSE.txt').read()
@@ -21,22 +20,26 @@ long_description = open('README.rst').read()
 
 
 setup(
-    name = 'django-simple-friends',
-    version = __version__,
-    url = 'http://github.com/muhuk/django-simple-friends',
-    author = __maintainer__,
-    author_email = __email__,
-    license = license_text,
-    packages = ['friends', 'friends.templatetags'],
-    package_data= {'friends': ['fixtures/*.json',
-                               'locale/*/LC_MESSAGES/django.*']},
+    name='django-simple-friends',
+    version=__version__,
+    url='http://github.com/muhuk/django-simple-friends',
+    author=__maintainer__,
+    author_email=__email__,
+    license=license_text,
+    packages=['friends', 'friends.templatetags'],
+    package_data={
+        'friends': ['fixtures/*.json', 'locale/*/LC_MESSAGES/django.*'],
+    },
     data_files=[('', ['LICENSE.txt', 'README.rst'])],
-    description = 'Like django-friends, but simpler',
+    description='Like django-friends, but simpler',
     long_description=long_description,
-    classifiers = ['Development Status :: 5 - Production/Stable',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content']
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=['Django >= 1.4.5'],
 )
