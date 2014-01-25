3ae296c38960cca5e4d5628ea6835e25ce71af9c
Christian Abbott <abbc1022@yahoo.com>
2014-01-25 11:06:06 -0800

    Improves lookup query in post_syncdb_handler, adds FRIENDS_SYNCDB_BATCH_SIZE setting, and removes created_model check in post_syncdb_handler so that it will still run if a user misconfigures that setting and then corrects the problem.

--- friends/app_settings.py
+++ friends/app_settings.py
@@ -4,3 +4,8 @@ from django.conf import settings
 REDIRECT_FALLBACK_TO_PROFILE = getattr(settings,
                                        'FRIENDS_REDIRECT_FALLBACK_TO_PROFILE',
                                        False)
+
+# During syncdb, this app ensures a Friendship and UserBlock record exists for
+# each user. This setting controls the batch size on the bulk creation of those
+# records when that process runs.
+FRIENDS_SYNCDB_BATCH_SIZE = getattr(settings, 'FRIENDS_SYNCDB_BATCH_SIZE', 999)
--- friends/management.py
+++ friends/management.py
@@ -1,35 +1,41 @@
 from django.db.models.signals import post_syncdb
 from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
 import models
+from app_settings import FRIENDS_SYNCDB_BATCH_SIZE
 
 
 def post_syncdb_handler(sender, app, created_models, verbosity, **kwargs):
-    friends_model_created = models.Friendship in created_models
-    block_model_created = models.UserBlocks in created_models
-    app_models = []
-    if friends_model_created:
-        app_models.append(models.Friendship)
-    if block_model_created:
-        app_models.append(models.UserBlocks)
-    if not app_models:
-        return
-    user_ids = User.objects.values_list('pk', flat=True).order_by('pk')
-    for model in app_models:
+    if FRIENDS_SYNCDB_BATCH_SIZE < 1:
+        raise ImproperlyConfigured("FRIENDS_SYNCDB_BATCH_SIZE must be > 0")
+    for model in (models.Friendship, models.UserBlocks):
         name = model.__name__
         if verbosity >= 1:
             print "Creating {0} models for existing users...".format(name)
-        existing_ids = model.objects.values_list('pk', flat=True)
-        # Convert to set for faster lookups: O(1) vs O(n)
-        existing_ids = set(existing_ids)
-        # We only want user IDs that don't have existing Friendship records.
-        # For example, a superuser account created interactively during syncdb
-        # will have a Friendship record due to this app's post_save signal
-        # handler
-        user_ids = [i for i in user_ids if i not in existing_ids]
+        # We only want user IDs that don't have existing Friendship or
+        # UserBlock records. For example, a superuser account created
+        # interactively during syncdb will have such a record due to this
+        # app's post_save signal handler
+        if model == models.Friendship:
+            kwargs = { 'friendship__isnull': True }
+        else:
+            kwargs = { 'user_blocks__isnull': True }
+        user_ids = User.objects.filter(**kwargs).values_list('pk', flat=True)\
+            .order_by('pk')
+        # Note: In Django 1.5, a batch_size parameter can be passed directly to
+        # bulk_create(). Better to use that parameter instead of the below
+        # code once version 1.4 is no longer supported by this app.
         batch = []
+        count = 0
         for user_id in user_ids:
             batch.append(model(user_id=user_id))
-        model.objects.bulk_create(batch)
+            count += 1
+            if count == FRIENDS_SYNCDB_BATCH_SIZE:
+                model.objects.bulk_create(batch)
+                batch = []
+                count = 0
+        if batch:
+            model.objects.bulk_create(batch)
         if verbosity >= 2 or True:
             print "Created {0} new {1} record(s).".format(len(user_ids), name)
 
